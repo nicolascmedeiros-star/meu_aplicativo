ebf178f6fd911b3ae70fd0e4cff6e1604d80f548
nicolascmedeiros-star <nicolas_c_medeiros@estudante.sesisenai.org.br>
2026-09-21 20:36:37 -0300

    app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("================================");
+console.log("MEU APLICATIVO");
+console.log("================================");
+
+console.log("Nome: Nicolas");
+console.log("Cidade: Schroeder");
+console.log("Atividade de interesse: jogos");
+console.log("Quero aprender: IA e desenvolvimento de jogos");
+console.log("Área de interesse: tecnologia");
+console.log("Mensagem: Quero me tornar um profissional nessa área de TI!");
