9454cf0acb44169ca8fb8c5718438f8f220a57c2
Nicolas <nicolas_c_medeiros@estudante.sesisenai.org.br>
2026-09-21 20:51:16 -0300

    Adiciona novo desafio

--- app.js
+++ app.js
@@ -8,3 +8,4 @@ console.log("Atividade de interesse: jogos");
 console.log("Quero aprender: IA e desenvolvimento de jogos");
 console.log("Área de interesse: tecnologia");
 console.log("Mensagem: Quero me tornar um profissional nessa área de TI!");
+console.log("Meu próximo desafio: criar um jogo simples usando JavaScript e HTML5.");
